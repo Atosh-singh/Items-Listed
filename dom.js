f5f9c9c657f8a2a02bb0096ca21d7ae915eac73e
Atosh-singh <atoshsingh7536@gmail.com>
2022-09-24 15:06:34 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,36 @@
+// EXAMINE THE DOCUMENT OBJECT
+
+//console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+
+// //document.title = "Hi Everybody";
+
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[18]);
+
+// //document.all[10].textContent = "Hello";
+
+// console.log(document.forms);
+// console.log(document.links);
+
+//console.log(document.images);
+
+//GETELEMENTBYID //
+
+//console.log(document.getElementById("header-title"));
+
+var headerTitle = document.getElementById("header-title");
+var header = document.getElementById("main-header");
+// headerTitle.textContent = "Item Lister2";
+// headerTitle.innerText = "Item lister4";
+//console.log(headerTitle);
+//headerTitle.innerHTML = "<h3>Hello</h3>";
+
+header.style.borderBottom = "solid 4px #000";
+//headerTitle.textContent = "Chal rha h ";
