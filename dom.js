3843a64ad509aabe58e3222610e6f14e727d2a83
Atosh-singh <atoshsingh7536@gmail.com>
2022-09-26 15:23:21 +0530

    parentNode

--- dom.js
+++ dom.js
@@ -102,6 +102,7 @@ var titles = document.querySelectorAll(".title");
 console.log(titles);
 
 var secondItem = document.querySelectorAll("li:nth-child(2)");
+//secondItem.style.color = "green";
 
 var odd = document.querySelectorAll("li:nth-child(odd)");
 var even = document.querySelectorAll("li:nth-child(even)");
@@ -109,3 +110,117 @@ for (var i = 0; i < odd.length; i++) {
   odd[i].style.backgroundColor = "green";
   even[i].style.backgroundColor = "#ccc";
 }
+// TRAVERSING THE DOM //
+
+//var itemList = document.querySelector("#items");
+// Parent Node
+
+//console.log(itemList.parentNode);
+
+// itemList.parentNode.style.backgroundColor = "#f4f4f4";
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+//Parent Element
+
+// console.log(itemList.parentElement);
+
+// itemList.parentElement.style.backgroundColor = "#f4f4f4";
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+// childNodes
+
+// console.log(itemList.childNodes);
+
+// children
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.color = "red";
+
+// // firstChild
+
+// console.log(itemList.firstChild);
+
+// // firstElemenntChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = "Hello1";
+
+// lastChild
+
+// console.log(itemList.lastChild);
+
+// // lastElemenntChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = "Hello4";
+
+//next
+// console.log(itemList.nextSibling);
+
+// // nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+// previousSibling
+// console.log(itemList.previousSibling);
+
+// // previousElementSibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = "green";
+
+// createElement
+
+// create a div
+let newDiv = document.createElement("div");
+// Add class
+newDiv.className = "Hello";
+
+// Add id
+newDiv.id = "hello1";
+
+// Add attribute
+newDiv.setAttribute("title", "Hello Div");
+
+// Create a text node
+var newDivText = document.createTextNode("Hello ");
+
+// Add text to div
+
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector("header .container");
+console.log(container);
+
+var h1 = document.querySelector("header h1");
+console.log(h1);
+
+container.insertBefore(newDiv, h1);
+
+// add before item1
+
+newDiv.style.fontSize = "30px";
+
+var itemList = document.querySelector("#items");
+console.log(itemList);
+
+// crteate a li element
+
+let newLi = document.createElement("li");
+
+// add class
+
+newLi.className = "task";
+
+// add id
+newLi.id = "task1";
+
+// add attributes
+newLi.setAttribute("title", "tasks");
+
+// create a textNode
+var newText = document.createTextNode("Helo");
+//console.log(newText);
+
+// append textnode or add
+newLi.appendChild(newText);
+
+// var itemListss = '<li>Helo</li>+itemList.innerHTML;
+// console.log(itemListss);
+// console.log(newLi);
